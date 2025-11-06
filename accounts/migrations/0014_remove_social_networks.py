3b323d162bc27ba12a7f1860290a5b69230e138d
reazonvan <ivanpetrov20066.ip@gmail.com>
2025-11-06 10:59:27 +0300

    fix: номер миграции переименован в 0014

--- accounts/migrations/0014_remove_social_networks.py
+++ accounts/migrations/0014_remove_social_networks.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0012_profile_last_seen'),
+        ('accounts', '0013_remove_profile_profile_last_seen_idx'),
     ]
 
     operations = [
